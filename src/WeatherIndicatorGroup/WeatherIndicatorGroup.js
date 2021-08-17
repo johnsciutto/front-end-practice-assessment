0cabc8a0302a688f5e4dcea3180c5a3cfc716c60
John Sciutto <johnsciutto@gmail.com>
2021-08-17 19:48:22 -0400

    Add key prop.

--- src/WeatherIndicatorGroup/WeatherIndicatorGroup.js
+++ src/WeatherIndicatorGroup/WeatherIndicatorGroup.js
@@ -5,8 +5,8 @@ const WeatherIndicatorGroup = ({ data }) => {
   return (
     <div className="indicator-group">
       {data.map((dataDay, idx) => {
-        if (idx === 0) return <WeatherIndicator today data={dataDay} />
-        return <WeatherIndicator data={dataDay} />
+        if (idx === 0) return <WeatherIndicator key={dataDay.date} today data={dataDay} />
+        return <WeatherIndicator key={dataDay.date} data={dataDay} />
       })}
     </div>
   );
