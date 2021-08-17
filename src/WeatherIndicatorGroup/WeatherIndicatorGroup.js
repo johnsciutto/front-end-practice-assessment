3928c39d620b62b6adcee806bdd413b519b543d7
John Sciutto <johnsciutto@gmail.com>
2021-08-17 17:13:32 -0400

    Use map to render multiple components.

--- src/WeatherIndicatorGroup/WeatherIndicatorGroup.js
+++ src/WeatherIndicatorGroup/WeatherIndicatorGroup.js
@@ -4,11 +4,10 @@ import './WeatherIndicatorGroup.css';
 const WeatherIndicatorGroup = ({ data }) => {
   return (
     <div className="indicator-group">
-      <WeatherIndicator today data={data[0]} />
-      <WeatherIndicator data={data[1]} />
-      <WeatherIndicator data={data[2]} />
-      <WeatherIndicator data={data[3]} />
-      <WeatherIndicator data={data[4]} />
+      {data.map((dataDay, idx) => {
+        if (idx === 0) return <WeatherIndicator today data={dataDay} />
+        return <WeatherIndicator data={dataDay} />
+      })}
     </div>
   );
 };
